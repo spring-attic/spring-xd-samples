9fcab59ff019fdf01177b7af3b76b2c4a2ee43c8
mpollack <mpollack@gopivotal.com>
2013-11-12 15:22:51 -0500

    Add copy file script

---
+++ batch-wordcount/copy-files.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+cp target/batch-wordcount-1.0.0.BUILD-SNAPSHOT-bin/modules/job/* $XD_HOME/modules/job
+cp target/batch-wordcount-1.0.0.BUILD-SNAPSHOT-bin/lib/* $XD_HOME/lib
+cp target/batch-wordcount-1.0.0.BUILD-SNAPSHOT-bin/nietzsche-chapter-1.txt /tmp
