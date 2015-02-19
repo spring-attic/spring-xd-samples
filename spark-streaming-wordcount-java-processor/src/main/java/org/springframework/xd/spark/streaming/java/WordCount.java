4277a36780eb4230f3244e3cf40e410424c72142
Ilayaperumal Gopinathan <igopinathan@pivotal.io>
2015-02-19 12:46:56 -0800

    Add spark streaming java/scala examples
     - Add word count example for processor implementations
     - Update README
     - Add separate projects for java/scala

---
+++ spark-streaming-wordcount-java-processor/src/main/java/org/springframework/xd/spark/streaming/java/WordCount.java
@@ -0,0 +1,72 @@
+/*
+ * Copyright 2015 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.springframework.xd.spark.streaming.java;
+
+import java.util.Arrays;
+import java.util.Properties;
+
+import org.apache.spark.api.java.function.FlatMapFunction;
+import org.apache.spark.api.java.function.Function2;
+import org.apache.spark.api.java.function.PairFunction;
+import org.apache.spark.streaming.api.java.JavaDStream;
+import org.apache.spark.streaming.api.java.JavaPairDStream;
+
+import org.springframework.xd.spark.streaming.SparkConfig;
+
+import scala.Tuple2;
+
+/**
+ * @author Mark Fisher
+ * @since 1.1
+ */
+@SuppressWarnings({ "serial" })
+public class WordCount implements Processor<JavaDStream<String>, JavaPairDStream<String, Integer>> {
+
+	@Override
+	public JavaPairDStream<String, Integer> process(JavaDStream<String> input) {
+		JavaDStream<String> words = input.flatMap(new FlatMapFunction<String, String>() {
+
+			@Override
+			public Iterable<String> call(String x) {
+				return Arrays.asList(x.split(" "));
+			}
+		});
+		JavaPairDStream<String, Integer> wordCounts = words.mapToPair(new PairFunction<String, String, Integer>() {
+
+			@Override
+			public Tuple2<String, Integer> call(String s) {
+				return new Tuple2<String, Integer>(s, 1);
+			}
+		}).reduceByKey(new Function2<Integer, Integer, Integer>() {
+
+			@Override
+			public Integer call(Integer i1, Integer i2) {
+				return i1 + i2;
+			}
+		});
+		return wordCounts;
+	}
+
+	@SparkConfig
+	public Properties getSparkConfigProperties() {
+		Properties props = new Properties();
+		// Any specific Spark configuration properties would go here.
+		// These properties always get the highest precedence
+		//props.setProperty(SPARK_MASTER_URL_PROP, "local[4]");
+		return props;
+	}
+}
---
+++ spark-streaming-wordcount-scala-processor/README.md
@@ -0,0 +1,61 @@
+Spring XD spark streaming processor example
+=================
+
+### Setup and configure spark cluster
+
+Spark streaming module can be run on any of the supporting cluster modes.
+You can either setup spark `standalone` cluster or use `mesos`, `yarn`. For instructions, see [spark cluster overview] (https://spark.apache.org/docs/1.2.1/cluster-overview.html) and [spark standalone setup] (https://spark.apache.org/docs/1.2.1/spark-standalone.html).
+
+For the development experience, you can use `local` mode as well.
+
+Once the spark cluster is setup, you can configure the property `spark.master` in XD servers.yml to use the corresponding cluster manager URL.The `spark.master` URL property can be configured at the module implementation using `@SparkConfig` annotation that returns Spark configuration properties:
+
+```
+  @SparkConfig
+  def properties : Properties = {
+    val props = new Properties()
+    props.setProperty("spark.master", "local[4]")
+    props
+  }
+```
+
+By default it is set to use `spark://localhost:7077`
+
+### Upload the module
+
+Spring XD provides uploading modules archive from the `shell` interface. We can use this approach to upload the word count example module into XD's module registry.
+
+1. Run the build to generate the jar
+
+  ```
+    ./gradlew clean build
+  ```
+  This will generate the spark-streaming-wordcount-scala-processor-0.1.0.jar under build/libs.
+  or, you can copy the upload-ready jar located at `.upload-ready-jar/`
+  
+2. Upload the generated jar into XD module registry
+
+  ```
+    module upload --file <location of the jar file>/spark-streaming-wordcount-scala-processor-0.1.0.jar 
+    --name scala-word-count --type processor
+  ```
+  
+### Deploy the stream
+
+1. Once the module is uploaded, we can start creating the stream
+
+  ```
+  stream create spark-streaming-word-count --definition "http | scala-word-count | log" --deploy
+  ```
+  
+2. Post messages to the stream
+
+  ```
+    http post --message "foo foo foo"
+  ```
+  
+3. The log sink module would show the word count results at the container log. This shows the word count computation happens at the spark cluster by the `java-word-count` module and the result is sent to the log module.
+
+  ```
+  .....  INFO xdbus.a1.1-1 sink.a1 - (foo,3)
+  ```
