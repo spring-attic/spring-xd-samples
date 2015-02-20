4338d69e6c75d258d0bb73a4ab271120de26af2c
Ilayaperumal Gopinathan <igopinathan@pivotal.io>
2015-02-20 11:40:36 -0800

    Replace functions with lambda expressions

--- spark-streaming-wordcount-java-processor/src/main/java/org/springframework/xd/spark/streaming/java/WordCount.java
+++ spark-streaming-wordcount-java-processor/src/main/java/org/springframework/xd/spark/streaming/java/WordCount.java
@@ -19,9 +19,6 @@ package org.springframework.xd.spark.streaming.java;
 import java.util.Arrays;
 import java.util.Properties;
 
-import org.apache.spark.api.java.function.FlatMapFunction;
-import org.apache.spark.api.java.function.Function2;
-import org.apache.spark.api.java.function.PairFunction;
 import org.apache.spark.streaming.api.java.JavaDStream;
 import org.apache.spark.streaming.api.java.JavaPairDStream;
 
@@ -31,33 +28,17 @@ import scala.Tuple2;
 
 /**
  * @author Mark Fisher
+ * @author Ilayaperumal Gopinathan
  * @since 1.1
  */
-@SuppressWarnings({ "serial" })
+@SuppressWarnings({"serial"})
 public class WordCount implements Processor<JavaDStream<String>, JavaPairDStream<String, Integer>> {
 
 	@Override
 	public JavaPairDStream<String, Integer> process(JavaDStream<String> input) {
-		JavaDStream<String> words = input.flatMap(new FlatMapFunction<String, String>() {
-
-			@Override
-			public Iterable<String> call(String x) {
-				return Arrays.asList(x.split(" "));
-			}
-		});
-		JavaPairDStream<String, Integer> wordCounts = words.mapToPair(new PairFunction<String, String, Integer>() {
-
-			@Override
-			public Tuple2<String, Integer> call(String s) {
-				return new Tuple2<String, Integer>(s, 1);
-			}
-		}).reduceByKey(new Function2<Integer, Integer, Integer>() {
-
-			@Override
-			public Integer call(Integer i1, Integer i2) {
-				return i1 + i2;
-			}
-		});
+		JavaDStream<String> words = input.flatMap(word -> Arrays.asList(word.split(" ")));
+		JavaPairDStream<String, Integer> wordCounts = words.mapToPair(s -> new Tuple2<String, Integer>(s, 1))
+				.reduceByKey((i1, i2) -> i1 + i2);
 		return wordCounts;
 	}
 
