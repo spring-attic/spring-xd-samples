a566c076a0ee11b9381b330e6b469798122cb3ea
Eric Bottard <eric.bottard+github@gmail.com>
2015-02-23 17:29:43 +0100

    Use latest boot features

--- smartgrid-prediction/frontend/src/main/java/io/pivotal/demo/smartgrid/frontend/SmartGridFrontend.java
+++ smartgrid-prediction/frontend/src/main/java/io/pivotal/demo/smartgrid/frontend/SmartGridFrontend.java
@@ -1,14 +1,12 @@
 package io.pivotal.demo.smartgrid.frontend;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.context.annotation.ComponentScan;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
  * @author Thomas Darimont
  */
-@ComponentScan
-@EnableAutoConfiguration
+@SpringBootApplication
 public class SmartGridFrontend {
 
     public static void main(String[] args) {
